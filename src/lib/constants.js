bd3feb57aa686da96a5a297525014e1abc6489de
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-08-02 23:56:40 +0500

    Building Surah Yaseen link

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 export const AUDIO_URLS = {
-  yaseen: "https://cdn.quranicaudio.com/quran/ar.alafasy/036.mp3",
-  kursi: "https://www.everyayah.com/data/Alafasy_64kbps/002255.mp3",
+  yaseen: "https://download.quranicaudio.com/quran/ar.alafasy/036.mp3",
+  kursi: "https://download.quranicaudio.com/quran/ar.alafasy/002255.mp3",
 };
 
 export const DEFAULT_PLAYBACK_RATE = 1.0;
